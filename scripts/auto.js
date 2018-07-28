0a52107a2c644112b44a0a451f751316a80bf4fa
Jackson Currie <30005345@g.toiohomai.ac.nz>
2018-07-28 15:43:46 +1200

    edit

--- scripts/auto.js
+++ scripts/auto.js
@@ -7,22 +7,8 @@
 // Socket
 var socket = io();
 
-var automate = (position) => {
+var pickup = () => {
 
-    if(position == "right") {
-
-        for(var i = 1450; i <= 2360; i++ ) {
-            socket.emit("rotate", i);
-        }
-
-    } else if(position == "left") {
-
-        for(var i = 1450; i >= 540; i-- ) {
-            socket.emit("rotate", i);
-        }
-
-    }
-    
     for(var i = 1250; i <= 1500; i++ ) {
         socket.emit("forward", i);
     }
@@ -35,10 +21,6 @@ var automate = (position) => {
         socket.emit("in", i);
     }
 
-    for(var i = 1100; i <= 1350; i++ ) {
-        socket.emit("in", i);
-    }
-
     for(var i = 900; i <= 1500; i++ ) {
         socket.emit("up", i);
     }
@@ -46,8 +28,38 @@ var automate = (position) => {
     for(var i = 1500; i >= 1250; i-- ) {
         socket.emit("forward", i);
     }
+}
 
-    for(var i = 2360; i >= 1450; i-- ) {
-        socket.emit("rotate", i);
+var automate = (position) => {
+
+    if(position == "right") {
+
+        for(var i = 1450; i <= 2360; i++ ) {
+            socket.emit("rotate", i);
+        }
+
+        pickup()
+
+        for(var i = 2360; i >= 1450; i-- ) {
+            socket.emit("rotate", i);
+        }
+
+    } else if(position == "left") {
+
+        for(var i = 1450; i >= 540; i-- ) {
+            socket.emit("rotate", i);
+        }
+
+        pickup()
+
+        for(var i = 540; i <= 1450; i++ ) {
+            socket.emit("rotate", i);
+        }
+
     }
+
+    for(var i = 1100; i <= 1350; i++ ) {
+        socket.emit("in", i);
+    }
+    
 }
