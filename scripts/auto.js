4577b335f57d03848ed6e42b161659b02da68142
Jackson Currie <30005345@g.toiohomai.ac.nz>
2018-07-28 15:18:56 +1200

    edit bug

--- scripts/auto.js
+++ scripts/auto.js
@@ -16,27 +16,27 @@ var automate = () => {
         socket.emit("forward", i);
     }
 
-    for(var i = 1500; i <= 900; i-- ) {
+    for(var i = 1500; i >= 900; i-- ) {
         socket.emit("up", i);
     }
 
-    for(var i = 1350; i <= 1100; i-- ) {
+    for(var i = 1350; i >= 1100; i-- ) {
         socket.emit("in", i);
     }
 
-    /*for(var i = 1100; i <= 1350; i-- ) {
+    for(var i = 1100; i <= 1350; i++ ) {
         socket.emit("in", i);
     }
 
-    for(var i = 900; i <= 1500; i-- ) {
+    for(var i = 900; i <= 1500; i++ ) {
         socket.emit("up", i);
     }
 
-    for(var i = 1500; i <= 1250; i++ ) {
+    for(var i = 1500; i >= 1250; i-- ) {
         socket.emit("forward", i);
     }
 
-    for(var i = 2360; i <= 1450; i++ ) {
+    for(var i = 2360; i >= 1450; i-- ) {
         socket.emit("rotate", i);
-    }*/
+    }
 }
